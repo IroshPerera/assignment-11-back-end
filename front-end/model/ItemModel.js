9d3f463e46dd6ac2b5ca76cdd14abbd6f023ac93
Irosh Perera <iroshlashan31@gmail.com>
2024-01-17 21:36:55 +0530

    few changes

---
+++ front-end/model/ItemModel.js
@@ -0,0 +1,8 @@
+export class ItemModel {
+    constructor(item_id, item_description, item_price, qty) {
+        this.item_id = item_id;
+        this.descr = item_description;
+        this.price = item_price;
+        this.qty = qty;
+    }
+}
